384d1d92b0a634a420b7dc1b499cd3a6f024b28e
dyann <dyanncameron@gmail.com>
2021-09-13 12:02:44 +1200

    3d obstacle

---
+++ Assets/Scripts/ScorePerSecond.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorePerSecond : MonoBehaviour{
+
+    public Text scoreText;
+    public float scoreAmount;
+    public float pointIncreasedPerSecond;
+    // Start is called before the first frame update
+    void Start(){
+        scoreAmount = 0f;
+        pointIncreasedPerSecond =1f;
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        scoreText.text = (int)scoreAmount + " Score";
+        scoreAmount +=pointIncreasedPerSecond = Time.deltaTime;
+
+        
+    }
+}
